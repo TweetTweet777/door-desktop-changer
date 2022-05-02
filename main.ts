e1f27efe926f947bb341b86bec5a394cbec6db5e
TweetTweet777 <48974347+TweetTweet777@users.noreply.github.com>
2022-05-03 11:35:55 +1200

    added counter to prevent looping code, added sound

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
 })
+let _set = 0
+input.calibrateCompass()
 keyboard.startKeyboardService()
 basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) < 100) {
-        keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + keyboard.modifiers(keyboard._Modifier.windows) + keyboard.keys(keyboard._Key.right), true)
-        basic.pause(100)
-        keyboard.releaseKeys()
+    if (input.magneticForce(Dimension.Strength) < 200) {
+        if (_set < 1) {
+            _set += 1
+            keyboard.sendSimultaneousKeys("" + keyboard.modifiers(keyboard._Modifier.control) + keyboard.modifiers(keyboard._Modifier.windows) + keyboard.keys(keyboard._Key.right), true)
+            music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+            basic.pause(100)
+            music.stopAllSounds()
+            keyboard.releaseKeys()
+        }
     } else {
-    	
+        _set = 0
     }
 })
